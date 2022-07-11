c232aa91e143bbae6ed54d916ad2aef63cdf7ebb
Andrew Huang <ktpss96034@gmail.com>
2022-07-11 10:32:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-/opt/dionaea/bin/dionaea -u root -g root -c /opt/dionaea/etc/dionaea/dionaea.cfg -D
+/opt/dionaea/bin/dionaea -u root -g root -c /opt/dionaea/etc/dionaea/dionaea.cfg
 python3 sniff.py 
