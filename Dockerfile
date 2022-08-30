1d71d61d8b13aec242f9fbfecc5e608e7f8e084f
Andrew Huang <ktpss96034@gmail.com>
2022-08-30 13:37:58 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -72,5 +72,7 @@ FROM zerotier/zerotier:1.8.7 AS zerotier
 
 COPY --from=python3 . ./
 
+USER dionaea:dionaea
+
 ENTRYPOINT ["/bin/bash", "-c", "/start.sh"]
 
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 zerotier-one -d
-/opt/dionaea/bin/dionaea -D
+/opt/dionaea/bin/dionaea -u dionaea -g dionaea -c /opt/dionaea/etc/dionaea/dionaea.cfg -D 
 python3 sniff.py
