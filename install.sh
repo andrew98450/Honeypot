451213ae901bf1b5b8c9f126c1bbb5edcbe21462
Andrew Huang <ktpss96034@gmail.com>
2022-09-03 09:48:13 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-sudo apt install python3 python3-pip autoconf git net-tools nano gcc wget tar iptables curl
-sudo pip3 install -r requirements.txt
-chmod +x start.sh
+sudo apt install python3 python3-pip autoconf git net-tools nano gcc wget tar iptables curl libtool
 git clone https://github.com/buffer/libemu.git
 cd libemu
 autoreconf -v -i
 ./configure
 make
 sudo make install
+cd ..
+sudo pip3 install -r requirements.txt
+chmod +x start.sh
 wget https://lcamtuf.coredump.cx/p0f3/releases/old/2.x/p0f-2.0.8.tgz
 tar xvf p0f-2.0.8.tgz
 sudo mkdir /opt/local
