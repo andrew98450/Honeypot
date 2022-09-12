5fee1aa64843bc29b8860b296c9cbb76085f2996
Andrew Huang <ktpss96034@gmail.com>
2022-09-12 21:50:00 +0800

    Add files via upload

--- modules.py
+++ modules.py
@@ -1,5 +1,7 @@
 import os
+import pickle
 import time
+from pyptables import *
 from scapy.all import *
 from scapy.layers.all import *
 from scapy.layers.http import *
@@ -18,30 +20,117 @@ ports = {21 : Protocol.FTP, 22: Protocol.SSH, 23 : Protocol.TELNET,
     2121: Protocol.CCPROXY, 3306: Protocol.MYSQL, 5900: Protocol.VNC,
     6000: Protocol.X11}
 emu = Emulator()
+tables = default_tables()
 syn_table = dict()
+
+if not os.path.exists('blacktable.filter'):
+    filted_table = dict()
+else:
+    filted_table = pickle.load(open('blacktable.filter', 'rb'))
+
+def default_setting(iface : str):
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 21 -j DNAT --to-destination 172.17.0.2:21" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 22 -j DNAT --to-destination 172.17.0.2:22" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 23 -j DNAT --to-destination 172.17.0.2:23" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 25 -j DNAT --to-destination 172.17.0.2:25" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p udp --dport 53 -j DNAT --to-destination 172.17.0.2:53" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 80 -j DNAT --to-destination 172.17.0.2:80" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 111 -j DNAT --to-destination 172.17.0.2:111" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 139 -j DNAT --to-destination 172.17.0.2:139" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 445 -j DNAT --to-destination 172.17.0.2:445" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 512 -j DNAT --to-destination 172.17.0.2:512" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 513 -j DNAT --to-destination 172.17.0.2:513" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 514 -j DNAT --to-destination 172.17.0.2:514" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 2049 -j DNAT --to-destination 172.17.0.2:2049" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 2121 -j DNAT --to-destination 172.17.0.2:2121" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 3306 -j DNAT --to-destination 172.17.0.2:3306" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 5432 -j DNAT --to-destination 172.17.0.2:5432" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 5900 -j DNAT --to-destination 172.17.0.2:5900" % iface)
+    os.system("sudo iptables -t nat -A PREROUTING -i %s -p tcp --dport 6000 -j DNAT --to-destination 172.17.0.2:6000" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 21 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 22 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 22 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 23 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 25 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p udp --dport 53 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 80 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 111 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 139 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 445 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 512 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 513 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 514 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 2049 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 2121 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 3306 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 5432 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 5900 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -t nat -A POSTROUTING -o %s -p tcp --dport 6000 --dst 172.17.0.2 -j MASQUERADE" % iface)
+    os.system("sudo iptables -A FORWARD -p tcp --dport 21 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 22 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 23 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 25 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p udp --dport 53 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 80 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 111 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 139 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 445 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 512 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 513 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 514 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 2049 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 2121 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 3306 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 5432 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 5900 -j ACCEPT")
+    os.system("sudo iptables -A FORWARD -p tcp --dport 6000 -j ACCEPT")
 
 def filter_blacklist(packet : Packet, blacklist_ref : db.Reference, iface : str):
     blacklist = blacklist_ref.get()
     if blacklist is None:
         blacklist = dict()
+    default_setting(iface)
+    inputs = tables['filter']['INPUT']
     if packet.haslayer(IP) and packet.haslayer(TCP):
         ip_field = packet[IP]
         tcp_field = packet[TCP]
         src_ip = str(ip_field.src)
         target_port = tcp_field.dport
         if src_ip.replace('.', '-') in blacklist.keys() and tcp_field.flags == 0x02:
-            os.system("sudo iptables -A INPUT -p tcp  -i %s -s %s --dport %d -j DROP" % (iface, src_ip, target_port))
+            drop_rule = Drop(i=iface, s=src_ip, dport=str(target_port), proto='tcp')
+            if src_ip not in filted_table.keys():
+                filted_table[src_ip] = {target_port: drop_rule}
+            if src_ip in filted_table.keys() and target_port not in filted_table[src_ip].keys():
+                filted_table[src_ip][target_port] = drop_rule
+            inputs.append(drop_rule)
         if src_ip.replace('.', '-') not in blacklist.keys() and tcp_field.flags == 0x02:
-            os.system("sudo iptables -A INPUT -p tcp  -i %s -s %s --dport %d -j ACCEPT" % (iface, src_ip, target_port))
+            accept_rule = Accept(i=iface, s=src_ip, dport=str(target_port), proto='tcp')
+            if src_ip in filted_table.keys():
+                inputs.remove(filted_table[src_ip][target_port])
+                filted_table.pop(src_ip)
+            inputs.append(accept_rule)
+        restore(tables)
+        pickle.dump(filted_table, open('blacktable.filter', 'wb'))
     elif packet.haslayer(IP) and packet.haslayer(UDP):
         ip_field = packet[IP]
         udp_field = packet[UDP]
         src_ip = str(ip_field.src)
         target_port = udp_field.dport
         if src_ip.replace('.', '-') in blacklist.keys():
-            os.system("sudo iptables -A INPUT -p udp -i %s -s %s --dport %d -j DROP" % (iface, src_ip, target_port))
+            drop_rule = Drop(i=iface, s=src_ip, dport=str(target_port), proto='udp')
+            if src_ip not in filted_table.keys():
+                filted_table[src_ip] = {target_port: drop_rule}
+            if src_ip in filted_table.keys() and target_port not in filted_table[src_ip].keys():
+                filted_table[src_ip][target_port] = drop_rule
+            inputs.append(drop_rule)
         if src_ip.replace('.', '-') not in blacklist.keys():
-            os.system("sudo iptables -A INPUT -p udp -i %s -s %s --dport %d -j ACCEPT" % (iface, src_ip, target_port))
+            accept_rule = Accept(i=iface, s=src_ip, dport=str(target_port), proto='udp')
+            if src_ip in filted_table.keys():
+                inputs.remove(filted_table[src_ip][target_port])
+                filted_table.pop(src_ip)
+            inputs.append(accept_rule)
+        restore(tables)
+        pickle.dump(filted_table, open('blacktable.filter', 'wb'))
 
 def get_information(packet : Packet, ref : db.Reference):
     info_ref = ref.child('info')
