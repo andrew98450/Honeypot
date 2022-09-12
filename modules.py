d0021bba669acec2c3c16563cc4ad5c39e63daff
Andrew Huang <ktpss96034@gmail.com>
2022-09-12 22:58:34 +0800

    Add files via upload

--- modules.py
+++ modules.py
@@ -1,7 +1,6 @@
 import os
 import pickle
 import time
-from pyptables import *
 from scapy.all import *
 from scapy.layers.all import *
 from scapy.layers.http import *
@@ -20,56 +19,30 @@ ports = {21 : Protocol.FTP, 22: Protocol.SSH, 23 : Protocol.TELNET,
     2121: Protocol.CCPROXY, 3306: Protocol.MYSQL, 5900: Protocol.VNC,
     6000: Protocol.X11}
 emu = Emulator()
-tables = default_tables()
 syn_table = dict()
-
-if not os.path.exists('blacktable.filter'):
-    filted_table = dict()
-else:
-    filted_table = pickle.load(open('blacktable.filter', 'rb'))
 
 def filter_blacklist(packet : Packet, blacklist_ref : db.Reference, iface : str):
     blacklist = blacklist_ref.get()
     if blacklist is None:
         blacklist = dict()
-    os.system("sudo iptables-restore < /opt/honeypot/iptables.conf")
-    inputs = tables['filter']['INPUT']
     if packet.haslayer(IP) and packet.haslayer(TCP):
         ip_field = packet[IP]
         tcp_field = packet[TCP]
         src_ip = str(ip_field.src)
         target_port = tcp_field.dport
         if src_ip.replace('.', '-') in blacklist.keys() and tcp_field.flags == 0x02:
-            drop_rule = Drop(i=iface, s=src_ip, dport=str(target_port), proto='tcp')
-            if src_ip not in filted_table.keys():
-                filted_table[src_ip] = {target_port: drop_rule}
-            if src_ip in filted_table.keys() and target_port not in filted_table[src_ip].keys():
-                filted_table[src_ip][target_port] = drop_rule
-            inputs.append(drop_rule)
+            os.system("sudo iptables -A INPUT -p tcp -i %s -s %s --dport %d -j DROP" % (iface, src_ip, target_port))
         if src_ip.replace('.', '-') not in blacklist.keys() and tcp_field.flags == 0x02:
-            if src_ip in filted_table.keys():
-                inputs.remove(filted_table[src_ip][target_port])
-                filted_table.pop(src_ip)
-        restore(tables)
-        pickle.dump(filted_table, open('blacktable.filter', 'wb'))
+            os.system("sudo iptables -A INPUT -p tcp -i %s -s %s --dport %d -j ACCEPT" % (iface, src_ip, target_port))
     elif packet.haslayer(IP) and packet.haslayer(UDP):
         ip_field = packet[IP]
         udp_field = packet[UDP]
         src_ip = str(ip_field.src)
         target_port = udp_field.dport
         if src_ip.replace('.', '-') in blacklist.keys():
-            drop_rule = Drop(i=iface, s=src_ip, dport=str(target_port), proto='udp')
-            if src_ip not in filted_table.keys():
-                filted_table[src_ip] = {target_port: drop_rule}
-            if src_ip in filted_table.keys() and target_port not in filted_table[src_ip].keys():
-                filted_table[src_ip][target_port] = drop_rule
-            inputs.append(drop_rule)
+            os.system("sudo iptables -A INPUT -p udp -i %s -s %s --dport %d -j DROP" % (iface, src_ip, target_port))
         if src_ip.replace('.', '-') not in blacklist.keys():
-            if src_ip in filted_table.keys():
-                inputs.remove(filted_table[src_ip][target_port])
-                filted_table.pop(src_ip)
-        restore(tables)
-        pickle.dump(filted_table, open('blacktable.filter', 'wb'))
+            os.system("sudo iptables -A INPUT -p udp -i %s -s %s --dport %d -j ACCEPT" % (iface, src_ip, target_port))
 
 def get_information(packet : Packet, ref : db.Reference):
     info_ref = ref.child('info')
@@ -471,13 +444,11 @@ def port_ack_scan_detect(packet : Packet, event_ref : db.Reference):
             })
 
 def sniffPacket(packet : Packet, connect_ref : db.Reference):
-    protocol = ''
     if packet.haslayer(IP):
         if packet.haslayer(TCP):
             ip_field = packet.getlayer(IP)
             tcp_field = packet.getlayer(TCP)
             target_port = tcp_field.dport
-           
             if target_port not in ports.keys():
                 protocol = 'other'
             elif ports[target_port] == Protocol.FTP:
